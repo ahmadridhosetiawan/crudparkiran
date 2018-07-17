ad8371b1d9338d1b66c23bdff2faa1cb5241a139
ahmadridhosetiawan <32403430+ahmadridhosetiawan@users.noreply.github.com>
2018-07-17 23:52:12 +0800

    crudparkiran

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('bootstrap/js/bootstrap.min.js') }}">
+@section('tipekendaraan')
+Daftar Tugas
+@endsection
+@section('nomerplat')
+<h1 align="center">WEBSITE PENCATAT PARKIRAN KENDARAAN</h1>
+@endsection
